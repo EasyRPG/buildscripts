a5f00526f765c0dfad7636d48d0c4fb8c91741eb
Ghabry <gabriel+github@mastergk.de>
2016-04-18 15:15:47 +0000

    Add Nintendo 3DS toolchain buildscripts

---
+++ 3ds/3_cleanup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+################################################################
+# Cleanup library build folders and other stuff
+
+rm -rf zlib-*/ freetype-*/ harfbuzz-*/ icu/ icu-native/ libmad-*/ libmodplug-*/ libogg-*/ libpng-*/ libvorbis-*/ pixman-*/ mpg123-*/ sdl-wii/ tremor-lowmem/ .patches-applied
+rm -rf ctrulib/ sf2dlib/ lpp-3ds_libraries/
+rm -f *.bz2 *.gz *.xz *.tgz *.pl
---
+++ 3ds/README.md
@@ -0,0 +1,17 @@
+# EasyRPG buildscripts
+
+## Nintendo 3DS Toolchain and libraries
+
+Specific building requirements for Linux:
+
+ - autotools
+ - git
+
+Local build process:
+
+Run `0_build_everything.sh` in a terminal
+
+Usage:
+
+Set environment variable `DEVKITPRO` to `$PWD/devkitPro`
+and `DEVKITARM` to `$DEVKITPRO/devkitARM`
