e1fa991b62e08aefbda5ed04369e755596edf2f2
Khelifi Mehdi <mehdi.abelha@gmail.com>
2016-07-09 20:49:43 +0200

    Android Clean 4_build_android_port.sh (#5)
    Android: Pull git repositories if already exist

--- android/4_build_android_port.sh
+++ android/4_build_android_port.sh
@@ -1,36 +1,44 @@
 #!/bin/bash
 
-#	Please complete :
+#	<!> Fill thoses variables :
 KEYSTORE_PATH=
 KEYSTORE_PASSWORD=
+##############################
 
 export WORKSPACE=$(pwd)
 export EASYDEV_ANDROID=$(pwd)
-#Number of CPU
+
+# Number of CPU
 NBPROC=$(getconf _NPROCESSORS_ONLN)
 
-#export ndk path
+# Export NDK path
 export NDK_ROOT=$WORKSPACE/android-ndk-r10e
 export PATH=$PATH:$NDK_ROOT
 
-#export sdk path
+# Export SDK path
 export SDK_ROOT=$WORKSPACE/android-sdk
 export ANDROID_HOME=$WORKSPACE/android-sdk
 export PATH=$PATH:$SDK_ROOT/tools:$SDK_ROOT/build-tools/23.0.2/
 
-git clone https://github.com/EasyRPG/Player.git
+# EasyRPG Player
+if [ -d Player/.git ]; then
+  git -C Player pull
+else
+  git clone https://github.com/EasyRPG/Player.git
+fi
 
 cd Player/builds/android
 ANDROID_FOLDER=$(pwd)
 
-# Update the Player
-git pull https://github.com/EasyRPG/Player.git
-
-# Obtain timidity (for midi player)
+# Timidity (midi player)
 cd $ANDROID_FOLDER/app/src/main
-git clone https://github.com/Ghabry/timidity_gus.git assets/timidity
+if [ -d assets/timidity/.git ]; then
+  git -C assets/timidity pull
+else
+  git clone https://github.com/Ghabry/timidity_gus.git assets/timidity
+fi
 
-#The target 1 should be API 12 if the user followed the script number 1 :
+# The target 1 should be API 12 if the user followed the script number 1 :
 android update project --path "." --target 1
 
 # Build
@@ -39,7 +47,7 @@ cd $ANDROID_FOLDER
 ./gradlew clean
 ./gradlew assembleRelease
 
-#Sign the apk
+# Sign the .apk
 cd $ANDROID_FOLDER/app/build/outputs/apk
 jarsigner -sigalg SHA1withRSA -digestalg SHA1 -keystore $KEYSTORE_PATH -storepass $KEYSTORE_PASSWORD app-release-unsigned.apk nightly
 zipalign 4 app-release-unsigned.apk EasyRpgPlayerActivity.apk
