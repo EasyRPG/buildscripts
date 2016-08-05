c16069323846d43824b05606cddf488d872eff9c
Ghabry <gabriel+github@mastergk.de>
2016-08-05 22:30:38 +0200

    Vita: Add toolchain deployment scripts

---
+++ vita/2_build_toolchain.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+
+# abort on error
+set -e
+
+export WORKSPACE=$PWD
+
+export PATH=$PWD/vitasdk/bin:$PATH
+
+export PLATFORM_PREFIX=$WORKSPACE
+export TARGET_HOST=arm-vita-eabi
+export PKG_CONFIG_PATH=$PLATFORM_PREFIX/lib/pkgconfig
+export PKG_CONFIG_LIBDIR=$PKG_CONFIG_PATH
+
+export VITASDK=$PWD/vitasdk
+
+# Number of CPU
+NBPROC=$(getconf _NPROCESSORS_ONLN)
+
+if [ ! -f .patches-applied ]; then
+	echo "patching libraries"
+
+	cp -r icu icu-native
+
+	# Fix ICU compilation problems
+	patch -Np0 < icu.patch
+
+	# disable pixman examples and tests
+	cd pixman-0.34.0
+	sed -i.bak 's/SUBDIRS = pixman demos test/SUBDIRS = pixman/' Makefile.am
+	autoreconf -fi
+	cd ..
+
+	# Fix broken load_abc.cpp
+	patch -Np0 < libmodplug.patch
+
+	# Fix mpg123 compilation
+	patch -Np0 < mpg123.patch
+
+	# Fix libsndfile compilation
+	patch -Np0 < libsndfile.patch
+
+	# Make CPU textures fast in libvita2d
+	sed -i.bak 's/SCE_KERNEL_MEMBLOCK_TYPE_USER_CDRAM_RW/SCE_KERNEL_MEMBLOCK_TYPE_USER_RW/g' libvita2d/libvita2d/source/vita2d_texture.c
+
+	touch .patches-applied
+fi
+
+function set_build_flags {
+	export CFLAGS="-I$WORKSPACE/include -g0 -O2"
+	export CPPFLAGS="$CFLAGS"
+	export LDFLAGS="-L$WORKSPACE/lib"
+}
+
+# Default lib installer
+function install_lib {
+	cd $1
+	./configure --host=$TARGET_HOST --prefix=$PLATFORM_PREFIX --disable-shared --enable-static $2
+	make clean
+	make -j1
+	make install
+	cd ..
+}
+
+# Install zlib
+function install_lib_zlib {
+	cd zlib-1.2.8
+	CHOST=$TARGET_HOST ./configure --static --prefix=$PLATFORM_PREFIX
+	make clean
+	make -j$NBPROC
+	make install
+	cd ..
+}
+
+# Install ICU
+function install_lib_icu() {
+	# Compile native version
+        unset CFLAGS
+        unset CPPFLAGS
+        unset LDFLAGS
+
+	cp icudt56l.dat icu/source/data/in/
+	cp icudt56l.dat icu-native/source/data/in/
+	cd icu-native/source
+	sed -i.bak 's/SMALL_BUFFER_MAX_SIZE 512/SMALL_BUFFER_MAX_SIZE 2048/' tools/toolutil/pkg_genc.h
+	chmod u+x configure
+	./configure --enable-static --enable-shared=no --enable-tests=no --enable-samples=no --enable-dyload=no --enable-tools --enable-extras=no --enable-icuio=no --with-data-packaging=static
+	make -j$NBPROC
+	export ICU_CROSS_BUILD=$PWD
+
+	# Cross compile
+	set_build_flags
+
+	cd ../../icu/source
+
+	cp config/mh-linux config/mh-unknown
+
+	chmod u+x configure
+	./configure --with-cross-build=$ICU_CROSS_BUILD --enable-strict=no --enable-static --enable-shared=no --enable-tests=no --enable-samples=no --enable-dyload=no --enable-tools=no --enable-extras=no --enable-icuio=no --host=$TARGET_HOST --with-data-packaging=static --prefix=$PLATFORM_PREFIX
+	make clean
+	make -j$NBPROC
+	make install
+	cd ../..
+}
+
+function install_lib_vita2d() {
+	# Provide freetype dependency
+	cp -r include/freetype2 $VITASDK/arm-vita-eabi/include/
+	
+	pushd libvita2d/libvita2d
+	make
+	make install
+	popd
+}
+
+set_build_flags
+# Install libraries
+install_lib_zlib
+install_lib "libpng-1.6.23"
+install_lib "freetype-2.6.3" "--with-harfbuzz=no"
+install_lib "pixman-0.34.0"
+install_lib "libogg-1.3.2"
+install_lib "libvorbis-1.3.5"
+install_lib "libmodplug-0.8.8.5"
+install_lib_icu
+install_lib "mpg123-1.23.3" "--enable-fifo=no --enable-ipv6=no --enable-network=no --enable-int-quality=no --with-cpu=generic --with-default-audio=dummy"
+install_lib "libsndfile-1.0.27"
+install_lib "speexdsp-1.2rc3"
+
+# Platform libs
+install_lib_vita2d
---
+++ vita/3_cleanup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+################################################################
+# Cleanup library build folders and other stuff
+
+rm -rf zlib-*/ freetype-*/ harfbuzz-*/ icu/ icu-native/ libmodplug-*/ libogg-*/ libpng-*/ libvorbis-*/ pixman-*/ mpg123-*/ libsndfile-*/ speexdsp-*/ .patches-applied
+rm -rf libvita2d/
+rm -f *.bz2 *.gz *.xz *.tgz *.pl icudt*
