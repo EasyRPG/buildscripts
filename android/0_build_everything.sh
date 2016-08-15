6cd94663e86be3db751379309c2c7ab5bc9da4f5
Carsten Teibes <dev@f4ke.de>
2016-08-15 02:56:51 +0200

    Android: posixify parent buildscript

--- android/0_build_everything.sh
+++ android/0_build_everything.sh
@@ -1,9 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-# abort on errors
-set -e
-
-./1_download_library.sh
-./2_build_toolchain.sh
-./3_build_liblcf.sh
-./4_build_android_port.sh
+./1_download_library.sh \
+	&& ./2_build_toolchain.sh \
+	&& ./3_build_liblcf.sh \
+	&& ./4_build_android_port.sh
