2b358ff59b847d908d819e96cb4c3b3f7d220f40
Ghabry <gabriel+github@mastergk.de>
2017-04-19 13:56:45 +0200

    MSVC: Update ICU, SDL2, libpng, libsndfile, mpg123 and pixman
    ICU patch removed because UWP is now officially supported

--- windows/build-icu.cmd
+++ windows/build-icu.cmd
@@ -11,8 +11,7 @@ IF "%EASYDEV_MSVC%"=="" SET EASYDEV_MSVC=%CD%\build
 
 set PATH=%CD%/msys/bin;%PATH%
 
-REM icudata 58 URL https://ci.easyrpg.org/job/icudata/lastSuccessfulBuild/artifact/icudata.tar.gz
-wget --no-check-certificate https://easyrpg.org/downloads/tmp/icudata56.tar.gz -O icudata.tar.gz
+wget --no-check-certificate https://ci.easyrpg.org/job/icudata/lastSuccessfulBuild/artifact/icudata.tar.gz
 tar xf icudata.tar.gz
 copy /Y icudt56l.dat projects\icu\source\data\in
 
