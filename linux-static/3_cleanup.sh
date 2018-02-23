d6be00d84659f781b58cc1cd6ca71c771165566f
Ghabry <gabriel+github@mastergk.de>
2018-02-23 21:01:12 +0100

    Update linux-static. Refactor.

--- linux-static/3_cleanup.sh
+++ linux-static/3_cleanup.sh
@@ -3,10 +3,9 @@
 echo
 echo "Cleaning up library build folders and other stuff..."
 
-rm -rf freetype-*/ harfbuzz-*/ icu/ libogg-*/ libpng-*/ libvorbis-*/ pixman-*/ \
-	mpg123-*/ libsndfile-*/ speexdsp-*/ SDL2-*/ SDL2_mixer-*/ SDL2_image-*/ \
-	expat-*/ libxmp-lite-*/ opus-*/ opusfile-*/ wildmidi-*/ zlib-*/
-rm -f *.zip *.bz2 *.gz *.xz *.tgz icudt* .patches-applied config.cache
-rm -rf bin/ sbin/ share/
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $SCRIPT_DIR/../shared/import
+
+cleanup
 
 echo " -> done"
