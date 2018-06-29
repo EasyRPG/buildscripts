e1ebc8833fe6368d2ed84a74160cfc3e07ec7bb8
Brian Saydak <brian.saydak@brianum.de>
2018-06-29 18:11:27 +0200

    Add missing optimization CFLAGS

--- android/2_build_toolchain.sh
+++ android/2_build_toolchain.sh
@@ -71,7 +71,7 @@ function build() {
 
 	export PATH=$PLATFORM_PREFIX/bin:$PATH
 
-	export CFLAGS="$5"
+	export CFLAGS="-g0 -O2 $5"
 	export CXXFLAGS=$CFLAGS
 	export CPPFLAGS="-I$PLATFORM_PREFIX/include -I$NDK_ROOT/sources/android/cpufeatures"
 	export LDFLAGS="-L$PLATFORM_PREFIX/lib"
