0108dd9bfdffee6b2a0e49fd6d49da1dc733b91a
Ghabry <gabriel+github@mastergk.de>
2024-03-29 20:42:04 +0100

    vcpkg: Specify freetype dependency for harfbuzz
    Is not the default anymore.

--- windows/build.cmd
+++ windows/build.cmd
@@ -6,7 +6,7 @@ call helper\prepare.cmd
 :: Using [core] everywhere to prevent surprises when new default-features are
 :: added to libraries.
 vcpkg install --triplet x86-windows-static --recurse^
- libpng[core] expat[core] pixman[core] freetype[core,zlib] harfbuzz[core]^
+ libpng[core] expat[core] pixman[core] freetype[core,zlib] harfbuzz[freetype]^
  libvorbis[core] libsndfile[core] wildmidi[core] libxmp[core]^
  speexdsp[core] mpg123[core] opusfile[core] fluidsynth-easyrpg[core]^
  inih[cpp] lhasa-easyrpg[core]^
@@ -14,7 +14,7 @@ vcpkg install --triplet x86-windows-static --recurse^
 
 :: Build 64-bit libraries
 vcpkg install --triplet x64-windows-static --recurse^
- libpng[core] expat[core] pixman[core] freetype[core,zlib] harfbuzz[core]^
+ libpng[core] expat[core] pixman[core] freetype[core,zlib] harfbuzz[freetype]^
  libvorbis[core] libsndfile[core] wildmidi[core] libxmp[core]^
  speexdsp[core] mpg123[core] opusfile[core] fluidsynth-easyrpg[core]^
  inih[cpp] lhasa-easyrpg[core]^
