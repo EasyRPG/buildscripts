4deb4fd9e2c929d7ca1659b20ad38d35e50111e1
Ghabry <1331889+Ghabry@users.noreply.github.com>
2024-10-18 18:27:24 +0000

    Update libraries

--- shared/packages.sh
+++ shared/packages.sh
@@ -17,18 +17,18 @@ FREETYPE_URL="https://mirrors.sarata.com/non-gnu/freetype/freetype-2.13.3.tar.xz
 FREETYPE_ARGS="-DFT_DISABLE_BZIP2=ON -DFT_DISABLE_BROTLI=ON"
 FREETYPE_DIR="freetype-2.13.3"
 
-HARFBUZZ_URL="https://github.com/harfbuzz/harfbuzz/releases/download/9.0.0/harfbuzz-9.0.0.tar.xz"
+HARFBUZZ_URL="https://github.com/harfbuzz/harfbuzz/releases/download/10.0.1/harfbuzz-10.0.1.tar.xz"
 HARFBUZZ_ARGS="-Dfreetype=enabled -Dtests=disabled -Dutilities=disabled" # TODO disable subset
-HARFBUZZ_DIR="harfbuzz-9.0.0"
+HARFBUZZ_DIR="harfbuzz-10.0.1"
 
 PIXMAN_URL="https://cairographics.org/releases/pixman-0.43.4.tar.gz"
 PIXMAN_ARGS="-Dtests=disabled -Ddemos=disabled -Dlibpng=disabled"
 PIXMAN_DIR="pixman-0.43.4"
 
-EXPAT_URL="https://github.com/libexpat/libexpat/releases/download/R_2_6_2/expat-2.6.2.tar.bz2"
+EXPAT_URL="https://github.com/libexpat/libexpat/releases/download/R_2_6_3/expat-2.6.3.tar.bz2"
 EXPAT_ARGS="-DEXPAT_BUILD_TOOLS=OFF -DEXPAT_BUILD_EXAMPLES=OFF \
 -DEXPAT_BUILD_TESTS=OFF -DEXPAT_BUILD_DOCS=OFF -DEXPAT_SHARED_LIBS=OFF"
-EXPAT_DIR="expat-2.6.2"
+EXPAT_DIR="expat-2.6.3"
 
 LIBOGG_URL="https://downloads.xiph.org/releases/ogg/libogg-1.3.5.tar.xz"
 LIBOGG_DIR="libogg-1.3.5"
@@ -103,8 +103,8 @@ ICU_ARGS="--enable-strict=no --disable-tests --disable-samples \
 ICUDATA_URL=https://ci.easyrpg.org/job/icudata/lastSuccessfulBuild/artifact/icudata.tar.gz
 ICUDATA_FILES="icudt*.dat"
 
-SDL2_URL="https://libsdl.org/release/SDL2-2.30.6.tar.gz"
-SDL2_DIR="SDL2-2.30.6"
+SDL2_URL="https://libsdl.org/release/SDL2-2.30.8.tar.gz"
+SDL2_DIR="SDL2-2.30.8"
 
 # 3.18.0, only needed for lmu2png tool
 FREEIMAGE_URL="https://github.com/carstene1ns/freeimage-easyrpg/archive/d82954e4adcb6c1b223bd3cb2e953b6bbf54dfcd.zip"
