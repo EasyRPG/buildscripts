2a21b83c9405b59ea488091571808f6448529811
Ghabry <gabriel+github@mastergk.de>
2025-11-24 12:38:09 +0100

    Windows: Update qt6 comment

--- windows/download_qt6.cmd
+++ windows/download_qt6.cmd
@@ -1,6 +1,6 @@
-:: Builds the dependencies for EasyRPG Editor Qt
-
-:: call helper\prepare.cmd
+:: Downloads the Qt6 precompiled libraries
+:: These are the official ones from the Qt Project
+:: Compiling Qt6 is not feasible as it takes hours
 
 curl -LO "https://github.com/miurahr/aqtinstall/releases/download/v3.3.0/aqt.exe"
 
